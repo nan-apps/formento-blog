9984850b2b56a550c76d198a95c7d8c96ac9ff47
Gaston Nan <nangastonr@gmail.com>
2018-02-11 19:56:20 -0300

    cambios en los meta data para fb

--- resources/lang/es/messages.php
+++ resources/lang/es/messages.php
@@ -5,7 +5,7 @@ return [
 	'read_more' => 'Seguir leyendo',
 	'reading_time' => 'Minutos de lectura',
 	'tagged_in' => 'Etiquetado en',
-	'last_post' => 'Ùltimo post',
+	'last_post' => 'Último post',
 	'customized_by' => 'Customizado por',
 	'powered_by' => 'Desarrollado por',
 	'code' => 'Código'
--- resources/views/vendor/canvas/frontend/shared/partials/meta.blade.php
+++ resources/views/vendor/canvas/frontend/shared/partials/meta.blade.php
@@ -13,11 +13,12 @@
 <link rel="icon" type="image/png" href="{{ asset('vendor/canvas/assets/images/favicon.png') }}">
 
 <!-- Facebook Open Graph Tags -->
+<meta property="og:url" content="@yield('og-url')">
 <meta property="og:title" content="@yield('og-title')">
 <meta property="og:image" content="@yield('og-image')">
 <meta property="og:image:width" content="800">
 <meta property="og:description" content="@yield('og-description')">
-<meta name="og:type" content="blog">
+<meta name="og:type" content="article">
 <meta name="og:site_name" content="{{ \Canvas\Models\Settings::blogTitle() }}">
 
 <!-- Twitter Cards -->
